c311c31f4f562494644499b6ae40ebcad0c4e603
Francesco <francesco@francescorega.eu>
2021-05-25 23:42:23 +0200

    Removed error confirmation prompt

--- app.py
+++ app.py
@@ -17,15 +17,8 @@ def main():
             response = connector.upload()
         print(response)
     except ConnectorException as e:
-        closeWithMessage(e)
-
-
-# Function to terminate the script by showing an informational message requesting confirmation
-def closeWithMessage(message, confirmation=True):
-    print(message)
-    if confirmation:
-        input("Hit enter to close")
-    sys.exit(1)
+        print(e)
+        sys.exit(1)
 
 
 # When the script is started, run the main function
